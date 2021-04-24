795a239d2648e92b8e82e263f69015fabcf08cf3
dychf <1353120323@qq.com>
2021-04-24 11:11:45 +0800

    tan

--- ssd/solver/build.py
+++ ssd/solver/build.py
@@ -1,11 +1,14 @@
 import torch
 
 from .lr_scheduler import WarmupMultiStepLR
+from sgd_tan import SGD_tan
 
 
 def make_optimizer(cfg, model, lr=None):
     lr = cfg.SOLVER.BASE_LR if lr is None else lr
-    return torch.optim.SGD(model.parameters(), lr=lr, momentum=cfg.SOLVER.MOMENTUM, weight_decay=cfg.SOLVER.WEIGHT_DECAY)
+    # return torch.optim.SGD(model.parameters(), lr=lr, momentum=cfg.SOLVER.MOMENTUM, weight_decay=cfg.SOLVER.WEIGHT_DECAY)
+    return torch.optim.SGD(model.parameters(), lr=lr, momentum=cfg.SOLVER.MOMENTUM,
+                           weight_decay=cfg.SOLVER.WEIGHT_DECAY)
 
 
 def make_lr_scheduler(cfg, optimizer, milestones=None):
